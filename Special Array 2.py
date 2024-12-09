fd9965d3687770011af7277204e2e55629806cc3
Rudra Pratap Singh <147605152+Rudra7009@users.noreply.github.com>
2024-12-09 19:07:26 +0530

    Create Special Array 2.py

---
+++ Special Array 2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        
+        runningSum = 0
+        partialSum = []
+        oddEven = None
+
+        for num in nums:
+            if num % 2 == oddEven:
+                runningSum += 1
+            oddEven = num % 2
+            partialSum.append(runningSum)
+
+        out = []
+
+        for start, end in queries:
+            if partialSum[start] == partialSum[end]:
+                out.append(True)
+            else:
+                out.append(False)
+        return out
